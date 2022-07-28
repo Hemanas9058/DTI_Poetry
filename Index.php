221bc2dc9416ae69e23abba85c0a95a92a8358d3
Hemanas9058 <78828656+Hemanas9058@users.noreply.github.com>
2022-07-29 10:32:40 +1200

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <meta name="description" content="A project that challenged users to difer computer generated poetry from hand written poetry.">
+    <meta name="keywords" content="Poetry, computer generated, free verse, haiku, limerick, acrostic, project">
+    <meta name="author" content="Shawn Hemana">
+    
+    <title>Poetry Project</title>
+    
+    <!-- link to css goes here -->
+    <link href="css/poetry_style.css" rel="stylesheet">
+    
+<!-- google fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lato&family=Libre+Baskerville:wght@700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="wrapper">
+
+        <header class="box">
+            <h1>Poetry Project</h1>
+        </header>
+
+        <nav class="box">
+            Navigation goes here
+        </nav>
+
+        <main class="box">
+            Main content goes here
+        </main>
+
+        <aside class="box">
+            Side bar is here
+        </aside>
+
+        <footer class="box">
+            &copy; SH 2022
+        </footer>
+    </div>
+
+</body>
+
+</html>
