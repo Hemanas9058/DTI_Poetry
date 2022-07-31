c34fa8e112c3ddcce95160d96b790c443687e4bc
Hemanas9058 <78828656+Hemanas9058@users.noreply.github.com>
2022-08-01 09:47:23 +1200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <meta name="description" content="A project that challenged users to difer computer generated poetry from hand written poetry.">
+    <meta name="keywords" content="Poetry, computer generated, free verse, haiku, limerick, acrostic, project">
+    <meta name="author" content="Shawn Hemana">
+    
+    <title>The Poetry Project</title>
+    
+    <!-- link to css goes here -->
+    <link href="css/poetry_style.css" rel="stylesheet">
+    
+<!-- google fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lato&family=Libre+Baskerville:wght@700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="wrapper">
+
+        <header class="box">
+            <h1>The Poetry Project</h1>
+        </header>
+
+        <nav class="box">
+
+            <a class="dropbtn" href="index.php">Home</a> |
+
+            <div class="dropdown">
+                <button class="dropbtn"><u>Poetry</u></button>
+                <div class="dropdown-content">
+                    <a href="#">Free Verse</a>
+                    <a href="#">Haiku</a>
+                    <a href="#">Limerick</a>
+                    <a href="#">Acrostic</a>
+                </div>  <!-- / end of dropdown-content -->
+            </div>  <!-- / dropdown -->
+
+            |
+
+            <a class="dropbtn" href="sources.php">Sources</a> |
+            <a class="dropbtn" href="contact.php">Contact</a> 
+
+        </nav>
+
+        <main class="box">
+
+            <h2>Home</h2>
+
+            <p>
+            Poetry is one of the things that makes us human.  Or is it?  This site features poems on nature.  You are invited to read the poems and then try and work out if they were written by a human or if they were created with a poetry generator.
+            </p>
+
+            <p>
+            This site features poems on nature.  You are invited to read the poems and then try and work out if they were written by a human or if they were created with a poetry generator.
+            </p>
+
+        </main>
+
+        <aside class="box">
+
+            <h2>Generators</h2>
+            
+            <p>
+            If you’d like to generate some poems for yourself, here are some online generators that you 
+            can use…
+            </p>
+
+            <ul>
+            <li><a href="https://www.poem-generator.org.uk/">Poem Generator</a></li>
+            <li><a href="http://www.languageisavirus.com/automatic_poetry_generator.php">Language is a Virus</a></li>
+            </ul>
+
+            <p>
+            Know of a poetry generator not listed above?  Please <a href="contact.php">let me know about it</a> and I will add it to the list.
+            </p>
+
+        </aside>
+
+        <footer class="box">
+            &copy; SH 2022
+        </footer>
+    </div>
+
+</body>
+
+</html>
